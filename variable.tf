859befa54ef8bfce0bd322565fa91e9ed31cc8b7
snehamanoharan <59600528+snehamanoharan@users.noreply.github.com>
2023-03-29 01:22:42 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "region"{
+   description="region value"
+   Default="us-east-1"
+}
+ variable "ami"{
+   description="ami id value"
+   default="ami-00c39f71452c08778"
+ }
+
+variable "access_key"{ }
+variable "secret_key"{ }
